029694e1741796e98cb3b32df09120e103ef66c9
wmooyong <weiherng.mooyong@student.unimelb.edu.au>
2023-04-19 15:43:57 +1000

    Initial attempt of adapting code to use Part A

--- agent/program.py
+++ agent/program.py
@@ -2,7 +2,7 @@
 # Project Part B: Game Playing Agent
 
 from referee.game import \
-    PlayerColor, Action, SpawnAction, SpreadAction, HexPos, HexDir
+    PlayerColor, Action, SpawnAction, SpreadAction, HexPos, HexDir, MAX_CELL_POWER
 
 
 # This is the entry point for your game playing agent. Currently the agent
@@ -11,28 +11,54 @@ from referee.game import \
 # intended to serve as an example of how to use the referee API -- obviously
 # this is not a valid strategy for actually playing the game!
 
+GRID_LAYOUT = "gridLayout"
+PREVIOUS_MOVES = "previousMoves"
+HEURISTIC_RESULT = "heuristicResult"
+GAME_ENDED = "gameEnded"
+
 class Agent:
+
+    grid = {}
+    
     def __init__(self, color: PlayerColor, **referee: dict):
         """
         Initialise the agent.
         """
         self._color = color
-        match color:
-            case PlayerColor.RED:
-                print("Testing: I am playing as red")
-            case PlayerColor.BLUE:
-                print("Testing: I am playing as blue")
+        # match color:
+        #     case PlayerColor.RED:
+        #         print("Testing: I am playing as red")
+        #     case PlayerColor.BLUE:
+        #         print("Testing: I am playing as blue")
 
     def action(self, **referee: dict) -> Action:
         """
         Return the next action to take.
         """
-        match self._color:
-            case PlayerColor.RED:
-                return SpawnAction(HexPos(3, 3))
-            case PlayerColor.BLUE:
-                # This is going to be invalid... BLUE never spawned!
-                return SpreadAction(HexPos(3, 3), HexDir.Up)
+        currentGrid = {PlayerColor.RED: {}, PlayerColor.BLUE: {}}
+        
+        # splitting cells for computation
+        for cell in self.grid.keys():
+            if self.grid[cell][0] == PlayerColor.RED:
+                currentGrid[PlayerColor.RED][cell] = self.grid[cell]
+            else:
+                currentGrid[PlayerColor.BLUE][cell] = self.grid[cell]
+
+        startingState = {GRID_LAYOUT: currentGrid, PREVIOUS_MOVES: [], 
+                     HEURISTIC_RESULT: [], GAME_ENDED: False}
+
+        bestStates = [startingState]
+        solution = False
+
+        
+
+        # match self._color:
+        #     case PlayerColor.RED:
+        #         return SpawnAction(HexPos(3, 3))
+        #     case PlayerColor.BLUE:
+        #         # This is going to be invalid... BLUE never spawned!
+        #         return SpreadAction(HexPos(3, 3), HexDir.Up)
+
 
     def turn(self, color: PlayerColor, action: Action, **referee: dict):
         """
@@ -40,8 +66,23 @@ class Agent:
         """
         match action:
             case SpawnAction(cell):
-                print(f"Testing: {color} SPAWN at {cell}")
+                self.grid[cell] = (color, 1)
                 pass
             case SpreadAction(cell, direction):
-                print(f"Testing: {color} SPREAD from {cell}, {direction}")
+                
+                for power in range(1, self.grid[cell][1] + 1):
+                    # if not in grid, spawn
+                    if (cell + power*direction) not in self.grid:
+                        self.grid[cell] = (color, 1)
+                    #in grid, add to power
+                    else: 
+                        # exceed maixmum, kill cell
+                        if self.grid[cell + power*direction] == MAX_CELL_POWER:
+                            self.grid.pop(cell + power*direction, None)
+                        # not exceed, add to power
+                        else:
+                            self.grid[cell + power*direction] = (color, self.grid[cell + power*direction][1] + 1)
+
+                # remove from record
+                self.grid.pop(cell, None)
                 pass

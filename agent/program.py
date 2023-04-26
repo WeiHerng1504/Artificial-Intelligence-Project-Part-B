131c5844dcf4605b192f0f3016d4f3c2a6d2b266
KhaiTan6 <khaisyuentan@hotmail.com>
2023-04-26 16:29:28 +0800

    progress to minimax implementation, logic not done

--- agent/program.py
+++ agent/program.py
@@ -1,6 +1,8 @@
 # COMP30024 Artificial Intelligence, Semester 1 2023
 # Project Part B: Game Playing Agent
 import numpy as np
+import copy
+import math
 from referee.game import \
     PlayerColor, Action, SpawnAction, SpreadAction, HexPos, HexDir, MAX_CELL_POWER
 
@@ -25,11 +27,11 @@ class Agent:
         Initialise the agent.
         """
         self._color = color
-        # match color:
-        #     case PlayerColor.RED:
-        #         print("Testing: I am playing as red")
-        #     case PlayerColor.BLUE:
-        #         print("Testing: I am playing as blue")
+        match color:
+            case PlayerColor.RED:
+                print("Testing: I am playing as red")
+            case PlayerColor.BLUE:
+                print("Testing: I am playing as blue")
 
     def action(self, **referee: dict) -> Action:
         """
@@ -50,14 +52,26 @@ class Agent:
         bestStates = [startingState]
         solution = False
 
-        
+        # for state in bestStates:
+            # potentialStates = potential_states(state, self._color)
 
-        # match self._color:
-        #     case PlayerColor.RED:
-        #         return SpawnAction(HexPos(3, 3))
-        #     case PlayerColor.BLUE:
-        #         # This is going to be invalid... BLUE never spawned!
-        #         return SpreadAction(HexPos(3, 3), HexDir.Up)
+        # changeable
+        depth = 3
+        # best_score, best_state = mini_max(potentialStates, depth, self._color)
+        best_score, best_state = mini_max(startingState, depth, self._color)
+        
+        if best_state != None:
+            best_move = best_state[PREVIOUS_MOVES][-1]
+
+        match self._color:
+            case PlayerColor.RED:
+                # return SpawnAction(HexPos(3, 3))
+                return SpawnAction(HexPos(best_move[0],best_move[1]))
+                # return SpreadAction(HexPos(best_move[0],best_move[0]), check enum for dir)
+            case PlayerColor.BLUE:
+                # This is going to be invalid... BLUE never spawned!
+                return SpreadAction(HexPos(3, 3), HexDir.Up)
+                # return SpawnAction(HexPos(3,2))
 
 
     def turn(self, color: PlayerColor, action: Action, **referee: dict):
@@ -86,35 +100,213 @@ class Agent:
                 # remove from record
                 self.grid.pop(cell, None)
                 pass
-    
-    def mini_max(grid, depth, color):
-        if depth == 0 """or leaf node """ :
-            return eval_func(grid)
-        
-        if color:
-            best_score = -np.inf
-            best_move = None
-            for move in potential_moves:
-                score = mini_max(move, depth-1, color != color)
-                if score > best_score:
-                    best_score = score
-                    best_move = move
-                
-                return best_score, best_move
-
-        else:
-            best_score = np.inf
-            best_move = None
-            for move in potential_moves:
-                score = mini_max(move, depth-1, color)
-                if score < best_score:
-                    best_score = score
-                    best_move = move
-
-                return best_score, best_move
 
     # return score for a particular grid state
-    def eval_func(grid):
-        score = 0
-        """not sure how to evaluate the grid and assign scores to it"""
-        return score
+
+def eval_func(state):
+    score = 0
+    # difference between num of reds and blues occupied
+    redCount = len(state[GRID_LAYOUT][PlayerColor.RED])
+    blueCount = len(state[GRID_LAYOUT][PlayerColor.BLUE])
+
+    # +ve means more red; -ve means more blue
+    score = redCount - blueCount
+    return score
+
+# takes a state, depth limit and colour; returns best score and best move
+def mini_max(state, depth, color: PlayerColor):
+    # game ended, no red hexes or no blue hexes
+    if depth == 0 or len(state[GRID_LAYOUT][PlayerColor.RED]) == 0 or len(state[GRID_LAYOUT][PlayerColor.BLUE]) == 0 :  
+        return eval_func(state), None
+    
+    if color:
+        best_score = -np.inf
+        best_move = None
+        for child in potential_states(state,color):
+            score = mini_max(child, depth-1, color != color)
+            if score > best_score:
+                best_score = score
+                best_move = child
+            
+            return best_score, best_move
+
+    else:
+        best_score = np.inf
+        best_move = None
+        for child in potential_states:
+            score = mini_max(child, depth-1, color)
+            if score < best_score:
+                best_score = score
+                best_move = state
+
+            return best_score, best_move
+
+    
+# state format
+# state = {gridLayout, previousMoves, heuristicResults, gameEnded}
+
+# return list of potential moves based on given state
+def potential_states( state, colour: PlayerColor):
+    potentialStates = []
+    validDirections = ((0, 1), (1, 0), (0, -1), (-1, 0), (1, -1), (-1, 1))
+
+    #dont need this for loop?
+    # for state in bestStates:
+        # for all red hexes
+    if colour == PlayerColor.RED:
+        for redHex in state[GRID_LAYOUT][PlayerColor.RED]:
+            #generate a possible future state
+            for direction in validDirections:
+                newState = generateState(state, redHex, direction,colour)
+                if newState:
+                    potentialStates.append(newState)
+    else:
+        for blueHex in state[GRID_LAYOUT][PlayerColor.BLUE]:
+            #generate a possible future state
+            for direction in validDirections:
+                newState = generateState(state, blueHex, direction, colour)
+                if newState:
+                    potentialStates.append(newState)
+    
+    return potentialStates
+    
+# Simulate a move. 
+# Takes a state, a hexagon location and movement direction as input.
+# Returns the simulated move as a new state
+def generateState(predecessor: dict[dict, list, list, bool], 
+                hex: tuple, direction: tuple, colour:PlayerColor):
+
+    # state format
+    # state = {gridLayout, previousMoves, heuristic_results, gameEnded}
+
+    heuristicResult = [0]
+    newState = copy.deepcopy(predecessor)
+    newGrid = newState["gridLayout"]
+
+    if colour == PlayerColor.RED:
+        for power in range(1, newGrid["redHexes"][hex][1] + 1):
+
+            r_new = (hex[0] + direction[0] * power) % 7
+            q_new = (hex[1] + direction[1] * power) % 7
+
+            # remove a blue hex
+            if (r_new, q_new) in newGrid["blueHexes"]:
+                heuristicResult[0] += 1
+                newGrid["redHexes"].update({(r_new, q_new): 
+                                (newGrid["blueHexes"][(r_new, q_new)])})
+                newGrid["blueHexes"].pop((r_new, q_new), None)
+
+            # update a red hex
+            if (r_new, q_new) in newGrid["redHexes"]:
+                if newGrid["redHexes"][(r_new, q_new)][1] < 6:
+                    newGrid["redHexes"].update({(r_new, q_new): 
+                                ('r', newGrid["redHexes"][(r_new, q_new)][1] + 1)})
+                else:
+                    newGrid["redHexes"].pop((r_new, q_new), None)
+            else:
+                newGrid["redHexes"].update({(r_new, q_new) : ('r', 1)})
+
+        # remove starting hex
+        newGrid["redHexes"].pop((hex))
+
+        # update heuristic
+        heuristicResult.append(heuristic(newGrid))
+
+        # state with no redhexes, avoid
+        if not newGrid["redHexes"]:
+            return None
+
+        # terminal state?
+        if not newGrid["blueHexes"]:
+            gameEnded = True
+        else:
+            gameEnded = False
+
+        newState["previousMoves"].append(hex + direction)
+
+        return {"gridLayout": newGrid, "previousMoves": newState["previousMoves"], 
+                "heuristicResult": heuristicResult, "gameEnded": gameEnded}
+    else:
+        for power in range(1, newGrid["blueHexes"][hex][1] + 1):
+
+            r_new = (hex[0] + direction[0] * power) % 7
+            q_new = (hex[1] + direction[1] * power) % 7
+
+            # remove a red hex
+            if (r_new, q_new) in newGrid["redHexes"]:
+                heuristicResult[0] += 1
+                newGrid["blueHexes"].update({(r_new, q_new): 
+                                (newGrid["redHexes"][(r_new, q_new)])})
+                newGrid["redHexes"].pop((r_new, q_new), None)
+
+            # update a blue hex
+            if (r_new, q_new) in newGrid["blueHexes"]:
+                if newGrid["blueHexes"][(r_new, q_new)][1] < 6:
+                    newGrid["blueHexes"].update({(r_new, q_new): 
+                                ('r', newGrid["blueHexes"][(r_new, q_new)][1] + 1)})
+                else:
+                    newGrid["blueHexes"].pop((r_new, q_new), None)
+            else:
+                newGrid["blueHexes"].update({(r_new, q_new) : ('r', 1)})
+
+        # remove starting hex
+        newGrid["blueHexes"].pop((hex))
+
+        # update heuristic
+        heuristicResult.append(heuristic(newGrid))
+
+        # state with no blueHexes, avoid
+        if not newGrid["blueHexes"]:
+            return None
+
+        # terminal state?
+        if not newGrid["redHexes"]:
+            gameEnded = True
+        else:
+            gameEnded = False
+
+        newState["previousMoves"].append(hex + direction)
+
+        return {"gridLayout": newGrid, "previousMoves": newState["previousMoves"], 
+                "heuristicResult": heuristicResult, "gameEnded": gameEnded}
+
+# red against blue
+def heuristic(layout: dict[dict, dict]):
+
+    # placeholder value to be replaced
+    shortestDistance = 1000
+
+    for blueHex in layout["blueHexes"].keys():
+        for redHex in layout["redHexes"].keys():
+
+            # manhattan distance with relative direction
+            manDist = [blueHex[0] - redHex[0], blueHex[1] - redHex[1]]
+
+            # check if wrapping is closer
+            for axis in range(len(manDist)):
+                if abs(manDist[axis]) > 3:
+                    if manDist[axis] < 0:
+                        manDist[axis] = manDist[axis] + 7
+                    else:
+                        manDist[axis] = manDist[axis] - 7
+
+            # vertical 
+            if manDist[0] != 0 and manDist[1] != 0 and manDist[0] / manDist[1] == -1:
+                distance = abs(manDist[0])
+            # closer vertically compared to horizontally
+            elif manDist[0] != 0 and manDist[1] != 0 and manDist[0] / manDist[1] < 0:
+                distance = math.sqrt( math.pow(manDist[0], 2) + math.pow(manDist[1], 2) - 
+                            2*abs(manDist[0])*abs(manDist[1])*math.cos(1/3 * math.pi))
+            # general case
+            else:
+                distance = math.hypot(abs(manDist[0]), abs(manDist[1]))
+
+            # longer, ignore
+            if distance > shortestDistance:
+                continue
+        
+            # shorter, keep track
+            if distance < shortestDistance:
+                shortestDistance = distance
+
+    return shortestDistance
